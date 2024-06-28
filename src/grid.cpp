237d20af77471997fa5db4c03c9d20ddf0b4d113
Anant <56.anant@gmail.com>
2024-06-29 07:52:36 +0900

    half

---
+++ src/grid.cpp
@@ -0,0 +1,65 @@
+#include "grid.h"
+#include <iostream>
+#include "colors.h"
+
+Grid::Grid()
+{
+    numRows = 20;
+    numCols = 10;
+    cellSize = 30;
+    Initilize();
+    colors = GetCellColors();
+}
+
+void Grid::Initilize()
+{
+    for(int row = 0; row < numRows; row++)
+    {
+        for(int column = 0; column < numCols; column++)
+        {
+            grid[row][column] = 0;
+        }
+    }
+}
+
+void Grid::Print()
+{
+    for(int row = 0; row < numRows; row++)
+    {
+        for(int column = 0; column < numCols; column++)
+        {
+            std::cout << grid[row][column]<< " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void Grid::Draw()
+{
+    for(int row =0; row < numRows; row++)
+    {
+        for(int column =0; column < numCols; column++)
+        {
+            int cellValue = grid[row][column];
+            DrawRectangle(column * cellSize+1, row * cellSize+1, cellSize-1, cellSize-1, colors[cellValue]); //(x, y, w, h, color)
+        }
+    }
+}
+
+bool Grid::IsCellOutside(int row, int column)
+{
+    if(row >= 0 && row < numRows && column >= 0 && column < numCols)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool Grid::isCellEmpty(int row, int column)
+{
+    if(grid[row][column] == 0)
+    {
+        return true;
+    }
+    return false;
+}
---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <raylib.h>
+#include "game.h"
+
+double lastUpdateTime = 0;
+
+bool EventTriggered(double interval)
+{
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    Color darkBlue = {44, 44, 127, 255};
+    InitWindow(300, 600, "GAIRE ANANTA PRASAD M24W0272");
+    SetTargetFPS(60); 
+
+    Game game = Game();     
+
+    while(WindowShouldClose() == false)
+    {
+        game.HandleInput();
+        if(EventTriggered(0.3))
+        {
+            game.MoveBlockDown();
+        }
+        BeginDrawing();
+        ClearBackground(darkBlue);
+        game.Draw();
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
